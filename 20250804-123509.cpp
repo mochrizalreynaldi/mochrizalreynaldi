f1e08e4327344103c275f95059606dc8deeeb7bd
mochrizalreynaldi <mochr2566@gmail.com>
2025-08-04 13:05:18 +0700

    Add files via upload

---
+++ 20250804-123509.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "moch rizal reynaldi" << endl;
+    cout << "umur 16" << endl;
+    cout << "Ma almanshuriyah" << endl;
+    cout << "XIPS" << endl; 
+    cout << "cipari pagelaran cianjur jawa barat" << endl;
+    return 0;
+}
