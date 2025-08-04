9a15f7bb50bedf34ce0f7953d9a9480a0d3a5bcf
mochrizalreynaldi <mochr2566@gmail.com>
2025-08-04 20:47:12 +0700

    Add files via upload

---
+++ 20250804-204211.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "aplikasi penjumlahan" << endl;
+    int bil1,bil2,hasil;
+    cout << "silakan masukan bil2";
+    cin >> bil2;
+    cout << "silakan msukan bil1";
+    cin >> bil1;
+    cout << hasil;
+    
+    
+    
+    
+    
+    
+}
